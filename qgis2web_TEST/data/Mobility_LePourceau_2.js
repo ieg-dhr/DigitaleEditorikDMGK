e59f8e1de77cd0e40c2763abdc7e427605785c65
Monika Barget <monika.barget@mu.ie>
2021-06-24 16:12:19 +0200

    Testing deployment GITHUB to GITLAB

---
+++ qgis2web_TEST/data/Mobility_LePourceau_2.js
@@ -0,0 +1,10 @@
+var json_Mobility_LePourceau_2 = {
+"type": "FeatureCollection",
+"name": "Mobility_LePourceau_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "marker-color": "#008040", "marker-size": "medium", "marker-symbol": "http:\/\/whgazetteer.org\/places\/12502346\/portal", "Bedeutung": "Geburtsort" }, "geometry": { "type": "Point", "coordinates": [ 5.834341049194335, 50.614399588024334 ] } },
+{ "type": "Feature", "properties": { "marker-color": "#008040", "marker-size": "medium", "marker-symbol": "http:\/\/whgazetteer.org\/places\/13902634\/portal", "Bedeutung": "Ausstellungsort" }, "geometry": { "type": "Point", "coordinates": [ 5.937080383300781, 50.612874608080858 ] } },
+{ "type": "Feature", "properties": { "marker-color": "#ff0000", "marker-size": "medium", "marker-symbol": "http:\/\/whgazetteer.org\/places\/13672534\/portal", "Bedeutung": "Zielort" }, "geometry": { "type": "Point", "coordinates": [ 8.26995849609375, 49.999353655995009 ] } }
+]
+}
