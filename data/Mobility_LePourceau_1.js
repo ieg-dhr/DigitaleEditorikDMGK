d7aaa2ae7b1eab05537414c61a6fe5c0cc62b8a9
Monika Barget <monika.barget@mu.ie>
2021-06-24 19:30:09 +0200

    Testing map v1

---
+++ data/Mobility_LePourceau_1.js
@@ -0,0 +1,8 @@
+var json_Mobility_LePourceau_1 = {
+"type": "FeatureCollection",
+"name": "Mobility_LePourceau_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "marker-color": null, "marker-size": null, "marker-symbol": null, "Bedeutung": null }, "geometry": { "type": "LineString", "coordinates": [ [ 5.834383964538574, 50.614467666333319 ], [ 5.9371018409729, 50.612969920775683 ], [ 8.26995849609375, 49.99968468472418 ] ] } }
+]
+}
